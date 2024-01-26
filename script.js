030005ee233ed921d77ce662939e2fa8e5a40558
kylepunta <kylepurcell1998@gmail.com>
2024-01-26 19:08:55 +0000

    Added a function to remove the grid and create a new grid with the number of squares equal to user input

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ squares.forEach((square) => {
         e.target.style.background = 'yellow';
     });
 });
+
+button.addEventListener('click', () => {
+    let newSquares = prompt("How many squares would you like per side?");
+
+    let rows = document.querySelectorAll('.flex-row');
+    rows.forEach((row) => {
+        row.remove();
+    });
+
+    for (let index = 1; index <= newSquares; index++) {
+        let row = document.createElement('div');
+        row.classList.add(`flex-row`);
+        mainContainer.appendChild(row);
+        for (let index = 1; index <= newSquares; index++) {
+            let square = document.createElement('div');
+            square.classList.add(`square`);
+            row.appendChild(square);
+        };
+    };
+
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', (e) => {
+            e.target.style.background = 'yellow';
+        });
+    });
+});
