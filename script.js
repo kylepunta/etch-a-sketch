72871b8226f8ddb416ef5e6d541398a90a83fc28
kylepunta <kylepurcell1998@gmail.com>
2024-01-26 17:25:02 +0000

    Added for loop to create 16 square divs and append them to the div container flex-row

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let row = document.createElement('div');
 row.classList.add('flex-row');
 
-let square = document.createElement('div');
-square.classList.add('square');
-
 let mainContainer = document.querySelector('.main-container');
 
-row.appendChild(square);
+for (let index = 0; index < 16; index++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    row.appendChild(square);
+};
+
 mainContainer.appendChild(row);
