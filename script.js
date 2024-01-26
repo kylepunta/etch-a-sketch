01b123abfb46d0d5ef1193e2722fbf5f81f6d7f9
kylepunta <kylepurcell1998@gmail.com>
2024-01-26 17:33:46 +0000

    Wrapped for loop to create squares inside the for loop to create rows. For each row created, 16 squares are created

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ let mainContainer = document.querySelector('.main-container');
 
 for (let index = 1; index <= 16; index++) {
     let row = document.createElement('div');
-    row.classList.add(`flex-row-${index}`);
+    row.classList.add(`flex-row`);
     mainContainer.appendChild(row);
+    for (let index = 1; index <= 16; index++) {
+        let square = document.createElement('div');
+        square.classList.add(`square`);
+        row.appendChild(square);
+    };
 };
-
-for (let index = 1; index <= 16; index++) {
-    let square = document.createElement('div');
-    square.classList.add(`square-${index}`);
-    row.appendChild(square);
-};
-
-mainContainer.appendChild(row);
