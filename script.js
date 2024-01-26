3672d526c636aa2110f4653cf32a25cfbc9e1ea3
kylepunta <kylepurcell1998@gmail.com>
2024-01-26 17:27:42 +0000

    Added a for loop to create 16 rows

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let row = document.createElement('div');
-row.classList.add('flex-row');
-
 let mainContainer = document.querySelector('.main-container');
+
+for (let index = 0; index < 16; index++) {
+    let row = document.createElement('div');
+    row.classList.add('flex-row');
+    mainContainer.appendChild(row);
+};
 
 for (let index = 0; index < 16; index++) {
     let square = document.createElement('div');
