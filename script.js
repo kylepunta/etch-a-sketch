212e1fb0f8d1ad5ee5b3c3243e564abdbd59498c
kylepunta <kylepurcell1998@gmail.com>
2024-01-26 17:30:09 +0000

    Changed index to 1 and added less than or equals operator. Added index position to class name

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let mainContainer = document.querySelector('.main-container');
 
-for (let index = 0; index < 16; index++) {
+for (let index = 1; index <= 16; index++) {
     let row = document.createElement('div');
-    row.classList.add('flex-row');
+    row.classList.add(`flex-row-${index}`);
     mainContainer.appendChild(row);
 };
 
-for (let index = 0; index < 16; index++) {
+for (let index = 1; index <= 16; index++) {
     let square = document.createElement('div');
-    square.classList.add('square');
+    square.classList.add(`square-${index}`);
     row.appendChild(square);
 };
 
